d9a66a2bbcccf51bd31daec725442cd7200181cf
0xD3ADCODE <atsumeru.dev@gmail.com>
2025-01-16 19:07:52 +0200

    Port to JavaFX

---
+++ src/test/java/xyz/fx/colorthief/RunTest.java
@@ -0,0 +1,11 @@
+package xyz.fx.colorthief;
+
+import javafx.application.Application;
+
+public class RunTest {
+
+    public static void main(String[] args) {
+        Application.launch(ColorThiefTest.class, args);
+        System.exit(0);
+    }
+}
